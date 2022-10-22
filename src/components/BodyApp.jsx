4d989de816f7f1bf30df872ef1bc3f58d446e00c
Jose Lecce <joselecce@gmail.com>
2022-10-22 18:34:48 -0300

    Primer Commit

---
+++ src/components/BodyApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function BodyApp (){
+
+    return (
+        <>
+            <h1>Tienda Copp - Tu tienda de Informatica</h1>
+            <p>Bienvenidos a la Tienda Informatica</p>
+        </>
+    
+        )
+}
